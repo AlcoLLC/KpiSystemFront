f60eac6326eaa9d89fa02b5ca556f94114e444fc
Aygun Amir <109809956+AmiraslanovaAygun@users.noreply.github.com>
2025-09-03 15:38:56 +0400

    home-charts

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,8 +1,8 @@
-import { Routes, Route } from "react-router-dom";
-import MainLayout from "../layouts/MainLayout";
-import Login from "../pages/Login";
-import Home from "../pages/Home";
-import PrivateRoute from "../components/PrivateRoute"; // -> PrivateRoute import edilir
+import { Routes, Route } from 'react-router-dom';
+import MainLayout from '../layouts/MainLayout';
+import Login from '../pages/Login';
+import Home from '../pages/Home';
+import PrivateRoute from '../components/PrivateRoute'; // -> PrivateRoute import edilir
 
 export default function AppRouter() {
   return (
